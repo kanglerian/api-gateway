a3369d8e562b01cf84fe80432f36a24f32e54767
kampusinformatika <kanglerian.business@gmail.com>
2022-06-15 10:57:48 +0700

    media CRD

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const coursesRouter = require('./routes/courses');
+const mediaRouter = require('./routes/media');
+const ordersRouter = require('./routes/orders');
+const paymentsRouter = require('./routes/payments');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/courses', coursesRouter);
+app.use('/media', mediaRouter);
+app.use('/orders', ordersRouter);
+app.use('/payments', paymentsRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ routes/media.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const mediaHandler = require('./handler/media');
+
+/* GET users listing. */
+router.get('/', mediaHandler.getAll);
+router.post('/', mediaHandler.create);
+router.delete('/:id', mediaHandler.destroy);
+
+module.exports = router;
